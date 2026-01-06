5005f5ad0e87b6907d1788e5709da393c0f174a7
RjGutierrezz <rbg002@uark.edu>
2026-01-05 16:42:55 -0800

    added menu component

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,6 +1,8 @@
 import CartButton from '@/components/CartButton'
+import MenuCard from '@/components/MenuCard'
 import { getCategories, getMenu } from '@/lib/appwrite'
 import useAppwrite from '@/lib/useAppwrite'
+import { MenuItem } from '@/type'
 import cn from 'clsx'
 import { useLocalSearchParams } from 'expo-router'
 import React, { useEffect } from 'react'
@@ -30,12 +32,13 @@ const Search = () => {
   return (
     <SafeAreaView className='bg-white h-full'>
       <FlatList data={data} renderItem={({item, index}) => {
+
+        // To cascade the items
         const isFirstRightColItem = index % 2 === 0;
-        
 
         return (
           <View className={cn('flex-1 max-w-[48%]', !isFirstRightColItem ? 'mt-10' : 'mt-0')}>
-            <Text>Menu Card</Text>
+            <MenuCard item={item as unknown as MenuItem}/>
           </View>
         )
       }} 
